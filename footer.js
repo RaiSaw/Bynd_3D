b5bf9e0e0e3b9a49a59bc2f79344caf5b78468fa
rai_S <raizadiannesawit@yahoo.com>
2023-11-16 21:15:22 +0100

    index.html in /root

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 let footer = `
     <footer class="footer d-flex flex-row flex-wrap justify-content-between align-items-center py-4 px-3 my-4 border-top">
       <div class="col d-flex space-x-4">
-        <a href="/Homepage/index.html" id='cont' class="text-body-secondary lh-1 rounded-circle">
+        <a href="/index.html" id='cont' class="text-body-secondary lh-1 rounded-circle">
           <img id="logo" class="h-8 w-auto" src="/Assets/bynd.png" alt="Logo">
         </a>
         <span class="text-body-secondary align-self-center">&copy; Bynd 2023</span>
@@ -9,7 +9,7 @@ let footer = `
       <div class="col d-flex space-x-4 justify-content-end">
       <!-- <p class="nav col-md-4 justify-content-center"><a href="#">Privacy</a> • <a href="#">Terms</a></p> -->
       <ul class="nav col d-flex align-items-center justify-content-end" style="text-decoration: none;">
-        <li class="nav-item"><a href="/Homepage/index.html" class="nav-link px-2 text-body-secondary">Home</a></li>
+        <li class="nav-item"><a href="/index.html" class="nav-link px-2 text-body-secondary">Home</a></li>
         <li class="nav-item"><a href="/Gallery/index.html" class="nav-link px-2 text-body-secondary">Gallery</a></li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
--- header.js
+++ header.js
@@ -1,14 +1,14 @@
 let nav = `
 <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary">
     <div class="container-fluid">
-      <a id="cont" class="text-body-secondary text-decoration-none lh-1 rounded-circle" href="/Homepage/index.html"><img class="h-8 w-auto" id="logo" src="/Assets/bynd.png" alt="Logo"></a>
+      <a id="cont" class="text-body-secondary text-decoration-none lh-1 rounded-circle" href="/index.html"><img class="h-8 w-auto" id="logo" src="/Assets/bynd.png" alt="Logo"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="text-decoration: none;">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="/Homepage/index.html">Home</a>
+            <a class="nav-link" aria-current="page" href="/index.html">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/Gallery/index.html">Gallery</a>
